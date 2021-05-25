af1fa5749d4c9741d889ec409cae07d4a377fb36
l775825 <834727979@qq.com>
2021-05-26 00:34:04 +0800

    路由配置

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,3 +1,29 @@
 <template>
-    <div>登录</div>
+  <div class="acount">
+    <div class="form-warp">
+      <a-form-model
+        ref="ruleForm"
+        v-bind="layout"
+      >
+        <a-form-model-item>
+          <a-input type="password" autocomplete="off" />
+        </a-form-model-item>
+
+        <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+          <a-button type="primary">
+            Submit
+          </a-button>
+        </a-form-model-item>
+      </a-form-model>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<style lang="scss">
+@import "./styles.scss";
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+    outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devDist',
+    lintOnSave: false,
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `@import "./src/styles/main.scss";`
+            },
+
+        }
+    }
+}
